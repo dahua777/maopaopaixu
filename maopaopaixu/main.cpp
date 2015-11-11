e16609051ebf476bdf792576754da8888abca46c
dahua777 <1220860737@qq.com>
2015-11-11 21:14:13 +0800

    Initial Commit

---
+++ maopaopaixu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maopaopaixu
+//
+//  Created by 陆天明 on 15/11/11.
+//  Copyright © 2015年 陆天明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
