2fa4128b4e68870412f88cfb875d73ef968b9d12
dahua777 <1220860737@qq.com>
2015-11-11 21:15:03 +0800

    冒泡排序
    第一次提交

--- maopaopaixu/main.cpp
+++ maopaopaixu/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#define N 5
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    int a[N];
+    int i,j,temp;
+    for (i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for (i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for (i=0;i<N ;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
